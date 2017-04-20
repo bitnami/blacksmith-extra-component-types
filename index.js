ee7525e058d20e423764afba70688663200bde1a
Andres <andres@bitnami.com>
2017-04-20 17:34:13 +0200

    Add specific exports for ruby 2.3 and node 7

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 module.exports = {
   RubyApplication: require('./ruby-application'),
   Ruby21Application: require('./ruby-application/ruby21'),
+  Ruby23Application: require('./ruby-application'),
   NodeApplication: require('./node-application'),
   Node4Application: require('./node-application/node4'),
+  Node7Application: require('./node-application'),
   PHPApplication: require('./php-application')
 };
