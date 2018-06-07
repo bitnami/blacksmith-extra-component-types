0b8c3cd58ac570f05507ade53f0aeb32a2980aba
Javier J. Salmerón-García <jsalmeron@bitnami.com>
2018-06-07 16:10:00 +0200

    Update index.js

--- ruby-application/index.js
+++ ruby-application/index.js
@@ -29,7 +29,7 @@ class RubyApplication extends CompilableComponent {
       {
         'type': 'nami',
         'id': 'ruby',
-        'installCommands': ['bitnami-pkg install ruby-2.3.4-0'],
+        'installCommands': ['bitnami-pkg install ruby-2.3.7-0'],
         'envVars': {
           PATH: '$PATH:/opt/bitnami/ruby/bin'
         }
