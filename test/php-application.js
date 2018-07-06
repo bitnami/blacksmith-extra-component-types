77f8cb8fa7c275501e5943ce78adc878cfa474fb
Javier J. Salmerón-García <jsalmeron@bitnami.com>
2018-07-06 11:47:05 +0200

    Update php-application.js

--- test/php-application.js
+++ test/php-application.js
@@ -93,11 +93,11 @@ describe('PHP Application', () => {
     const phpRuntimeDependencies = {
       debian: [
         'libbz2-1.0', 'libc6', 'libcomerr2', 'libcurl3', 'libffi6', 'libfreetype6', 'libgcc1', 'libgcrypt20',
-        'libgmp10', 'libgnutls-deb0-28', 'libgpg-error0', 'libgssapi-krb5-2', 'libhogweed2', 'libicu52',
+        'libgmp10', 'libgnutls28-dev', 'libgpg-error0', 'libgssapi-krb5-2', 'libhogweed4', 'libicu57',
         'libidn11', 'libjpeg62-turbo', 'libk5crypto3', 'libkeyutils1', 'libkrb5-3', 'libkrb5support0',
-        'libldap-2.4-2', 'liblzma5', 'libmcrypt4', 'libncurses5', 'libnettle4', 'libp11-kit0', 'libpng12-0',
-        'libpq5', 'libreadline6', 'librtmp1', 'libsasl2-2', 'libssh2-1', 'libssl1.0.0', 'libstdc++6',
-        'libsybdb5', 'libtasn1-6', 'libtidy-0.99-0', 'libtinfo5', 'libxml2', 'libxslt1.1', 'zlib1g',
+        'libldap-2.4-2', 'liblzma5', 'libmcrypt4', 'libncurses5', 'nettle-dev', 'libp11-kit0', 'libpng-dev',
+        'libpq5', 'libreadline-dev', 'librtmp1', 'libsasl2-2', 'libssh2-1', 'libssl1.0-dev', 'libstdc++6',
+        'libsybdb5', 'libtasn1-6', 'libtidy-dev', 'libtinfo5', 'libxml2', 'libxslt1.1', 'zlib1g',
       ],
       centos: [
         'bzip2-libs', 'cyrus-sasl-lib', 'freetype', 'glibc', 'gmp', 'keyutils-libs', 'krb5-libs', 'libcom_err',
