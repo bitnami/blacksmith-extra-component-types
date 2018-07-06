9876e12feb31acc617526ce3f29152f3ea5e2c5b
Javier J. Salmeron Garcia <jsalmeron@bitnami.com>
2018-07-06 15:33:35 +0200

    Lint

--- test/php-application.js
+++ test/php-application.js
@@ -93,7 +93,7 @@ describe('PHP Application', () => {
     const phpRuntimeDependencies = {
       'debian-8': [
         'libbz2-1.0', 'libc6', 'libcomerr2', 'libcurl3', 'libffi6', 'libfreetype6', 'libgcc1', 'libgcrypt20',
-        'libgmp10', 'libgpg-error0', 'libgssapi-krb5-2', 
+        'libgmp10', 'libgpg-error0', 'libgssapi-krb5-2',
         'libidn11', 'libjpeg62-turbo', 'libk5crypto3', 'libkeyutils1', 'libkrb5-3', 'libkrb5support0',
         'libldap-2.4-2', 'liblzma5', 'libmcrypt4', 'libncurses5', 'libp11-kit0',
         'libpq5', 'libreadline-dev', 'librtmp1', 'libsasl2-2', 'libssh2-1', 'libstdc++6',
@@ -127,7 +127,7 @@ describe('PHP Application', () => {
 
     const getBuildDependencies = (deps, distro, version) => _.map(_.filter(deps, bd => bd.distro === distro &&
       (!version || !bd.version || version === bd.version)), bd => bd.id);
-    
+
     const phpBuildDependencies = {
       'debian-8': getBuildDependencies(phpApplication.buildDependencies, 'debian', 8),
       'debian-9': getBuildDependencies(phpApplication.buildDependencies, 'debian', 9),
