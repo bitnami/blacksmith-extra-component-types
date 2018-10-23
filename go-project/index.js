60b52b4b9d6afe702b523196e14013abea0e0e55
Alejandro Ruiz <alexrwave@gmail.com>
2018-10-23 13:12:40 +0200

    Add GoProject component (#25)
    This component is based on the MakeComponent, but will ensure the latest stable version of Go is available.

---
+++ go-project/index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const nfile = require('nami-utils').file;
+const nos = require('nami-utils').os;
+const MakeComponent = require('blacksmith/lib/base-components').MakeComponent;
+
+class GoProject extends MakeComponent {
+  get goImportPrefix() {
+    throw new Error('unknown import path');
+  }
+
+  get goPath() {
+    return super.srcDir;
+  }
+
+  get srcDir() {
+    return nfile.join(this.goPath, 'src', this.goImportPrefix);
+  }
+
+  getExportableEnvironmentVariables() {
+    return Object.assign(super.getExportableEnvironmentVariables(), {
+      PATH: `${process.env.PATH}:${this.goPath}/bin:/usr/local/go/bin`,
+      GOPATH: this.goPath,
+    });
+  }
+
+  get goVersion() {
+    const latestStable = nos.runProgram(
+      'curl', ['-L', 'https://golang.org/VERSION?m=text']
+    ).replace(/^go/, '');
+    return latestStable;
+  }
+
+  get goBuildDependencies() {
+    return [];
+  }
+
+  get buildDependencies() {
+    const goTar = `go${this.goVersion}.linux-amd64.tar.gz`;
+    const goGetCommands = this.goBuildDependencies.map((dep) => `go get ${dep}`);
+    return [
+      {
+        type: 'go',
+        id: 'go',
+        installCommands: [
+          `curl https://dl.google.com/go/${goTar} -o ${goTar}`,
+          `tar -C /usr/local -xzf ${goTar}`,
+        ].concat(goGetCommands),
+      }
+    ];
+  }
+}
+
+module.exports = GoProject;
---
+++ test/go-project.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-unused-expressions */
+'use strict';
+
+const GoProject = require('../go-project');
+const helpers = require('blacksmith/test/helpers');
+const chai = require('chai');
+const chaiFs = require('chai-fs');
+const expect = chai.expect;
+chai.use(chaiFs);
+
+describe('GoProject', function() {
+  this.timeout(5000);
+  before('prepare environment', () => {
+    helpers.cleanTestEnv();
+  });
+  afterEach('clean environment', () => {
+    helpers.cleanTestEnv();
+  });
+  it('should return its buildDependencies', () => {
+    const component = new GoProject();
+    expect(component.buildDependencies).not.to.be.empty;
+    expect(component.buildDependencies.filter(d => d.id === 'go')).not.to.be.empty;
+  });
+  it('builds a sample go project', () => {
+    const log = {};
+    const test = helpers.createTestEnv();
+    const component = helpers.createComponent(test);
+    const goProject = new GoProject({
+      version: component.version,
+      id: component.id,
+      licenses: [{
+        type: component.licenseType,
+        licenseRelativePath: component.licenseRelativePath,
+        main: true
+      }]
+    }, {logger: helpers.getDummyLogger(log)});
+    goProject.id = component.id;
+    goProject.sourceTarball = component.sourceTarball;
+    Object.defineProperty(goProject, 'goBuildDependencies', {get: () => ['github.com/tools/godep']});
+    Object.defineProperty(goProject, 'goImportPrefix', {get: () => 'github.com/test/test'});
+
+    // mock make
+    goProject.make = () => true;
+
+    // Build process
+    const be = helpers.getDummyBuildEnvironment(test);
+    goProject.setup({be});
+    goProject.cleanup();
+    goProject.extract();
+    goProject.copyExtraFiles();
+    goProject.patch();
+    goProject.postExtract();
+    goProject.build();
+    goProject.postBuild();
+    goProject.install();
+    goProject.fulfillLicenseRequirements();
+    goProject.postInstall();
+  });
+
+  it('throws an error if goImportPrefix is not overriden', () => {
+    const component = new GoProject();
+    expect(() => component.goImportPrefix()).to.throw();
+  });
+});
