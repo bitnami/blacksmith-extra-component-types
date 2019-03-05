46907061b9239909b45a1113ec6c11c2441f1b74
juan131 <juan@bitnami.com>
2019-03-05 13:27:32 +0100

    Overwriting extract method

--- wordpress-plugin/index.js
+++ wordpress-plugin/index.js
@@ -1,16 +1,30 @@
 'use strict';
 
 const nfile = require('nami-utils').file;
+const path = require('path');
+const _ = require('nami-utils/lodash-extra');
+const tarballUtils = require('tarball-utils');
 const CompiledComponent = require('blacksmith/lib/base-components').CompiledComponent;
 
 class WordPressPlugin extends CompiledComponent {
   /**
    * Get build prefix
-   * @returns {mixed}
   */
   get prefix() {
-    const pluginDirname = this.id.replace(/wordpress-plugin-/g,'');
-    return nfile.join(this.be.prefixDir, 'wordpress', 'wp-content', 'plugins', pluginDirname);
+    return nfile.join(this.be.prefixDir, 'wordpress', 'wp-content', 'plugins');
+  }
+  /**
+  * Extract component tarball in srcDir
+  */
+  extract() {
+    if (_.isEmpty(this.source.tarball)) {
+      throw new Error(`The source tarball is missing. Received ${this.source.tarball}`);
+    }
+    if (!path.isAbsolute(this.source.tarball)) {
+      throw new Error(`Path to source tarball should be absolute. Found ${this.source.tarball}`);
+    }
+    this._validateChecksum(this.source.tarball, this.source.sha256);
+    tarballUtils.unpack(this.source.tarball, this.srcDir);
   }
 }
 
