cd193ecd2758da1d1ef43b3a100de9039153c438
Javier J. Salmeron Garcia <jsalmeron@vmware.com>
2020-02-21 10:46:50 +0100

    Add PHP74Application
    Signed-off-by: Javier J. Salmeron Garcia <jsalmeron@vmware.com>

---
+++ php-application/php74.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const PHPApplication = require('./index');
+
+/**
+ * Class representing a PHP Application for versions 7.4.X
+ * @namespace BaseComponents.PHP72Application
+ * @class
+ * @extends PHPApplication
+ */
+class PHP74Application extends PHPApplication {
+  get phpVersion() {
+    return '7.4.3-0';
+  }
+}
+
+module.exports = PHP74Application;
